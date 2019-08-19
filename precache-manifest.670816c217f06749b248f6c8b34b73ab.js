07cf7cc8c8d87d302e3518e89d19e51cd0b462e9
Adetula Tolulope <adetulatolu@gmail.com>
2019-08-19 18:45:45 +0100

    Updates

---
+++ precache-manifest.670816c217f06749b248f6c8b34b73ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04164d25f0609f4d79c823d7dcc3a9f",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "915c8f040cd5bb1e454f",
+    "url": "/Monster-rolodex/static/css/main.7522ca03.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/Monster-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "915c8f040cd5bb1e454f",
+    "url": "/Monster-rolodex/static/js/main.526a1d5c.chunk.js"
+  },
+  {
+    "revision": "e97fe8cff68dca64336e",
+    "url": "/Monster-rolodex/static/js/runtime~main.d35bb185.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
